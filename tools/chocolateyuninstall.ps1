f27f94ed3e5e2e30ab15d9118b38dd02536a0051
Robert Palethorpe <palethorper@gmail.com>
2016-09-23 15:05:23 +1000

    unistall updates and fix of nuspec packaging

--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -1,3 +1,9 @@
 ﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Invoke-Expression $(Join-Path $toolsDir "topbeat-1.3.1-windows\uninstall-service-topbeat.ps1")
-Uninstall-ChocolateyZipPackage '__NAME__' "topbeat.1.3.1.nupkg"
+$nupkgFile = Get-Content $(Join-Path $toolsDir "..\*.nupkg")
+$zipManifest = Get-Content $(Join-Path $toolsDir "..\*.zip.txt")
+$installPath = $zipManifest | select -First 1
+
+$uninstallScript = $zipManifest | where { $_ -imatch 'uninstall.*\.ps1' }
+
+Invoke-Expression $uninstallScript
+Uninstall-ChocolateyZipPackage '__NAME__' "$($nupkgFile)"
